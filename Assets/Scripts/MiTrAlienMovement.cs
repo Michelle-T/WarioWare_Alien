e65b56acbd48f10ac65b6c29757391c790f124b9
Michelle T <42843557+Michelle-T@users.noreply.github.com>
2018-12-01 23:38:09 -0500

    New Import

---
+++ Assets/Scripts/MiTrAlienMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiTrAlienMovement : MonoBehaviour {
+
+    public float speed;
+
+    //public GameObject.tag isGround;
+
+    public Transform wallHitBox;
+
+    private bool wallHit = false;
+
+    public float wallHitHeight;
+    public float wallHitWidth;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//transform.Position(new Vector3())
+	}
+
+    void FixedUpdate()
+    {
+
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        //wallHit is a bool similar to the ground one in the player code.
+        //Physics2D.OverlapBox is similar to Physics2D.OverlapCircle but uses a box
+        //The next is a Vector 2 with the box's Width and Height which are floats that I made public so I could edit them in the editor. 
+        //The zero is the z value we don't need.
+        //isGround is a LayerMask of everything that is ground.
+        //wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+
+        /*if (wallHit == true)
+        {
+            speed = speed * -1;
+        }*/
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            speed = speed * -1;
+            Debug.Log("I loved living");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(wallHitBox.position, new Vector3(wallHitWidth, wallHitHeight, 1));
+    }
+}
---
+++ Assets/Scripts/MiTrPlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiTrPlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private int count;
+
+    private float timer;
+    private int wholetime;
+
+    public Text endText;
+    public Text winText;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        endText.text = "";
+        SetCountText();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        rb.AddForce(movement * speed);
+
+        //This does a timer before ending the game after 10 seconds.
+        timer = timer + Time.deltaTime;
+        if (timer >= 10)
+        {
+            endText.text = "You Lose! T-T";
+            StartCoroutine(ByeAfterDelay(2));
+
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Alien"))
+        {
+            other.gameObject.SetActive(false);
+
+            //Add one to the current value of our count variable.
+            count = count + 1;
+
+            // add a point to the game
+            GameLoader.AddScore(1);
+
+            //Update the currently displayed count by calling the SetCountText function.
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        //Check if we've collected all 10 pickups. If we have...
+        if (count >= 1)
+        {
+            endText.text = "You win!";
+            StartCoroutine(ByeAfterDelay(2));
+        }
+    }
+
+    IEnumerator ByeAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        GameLoader.gameOn = false;
+    }
+}
